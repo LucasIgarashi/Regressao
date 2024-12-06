d02505dc7b640760fd426a295250ee2c85b676e7
DanielPorto09 <daniel.estudanteufsc@gmail.com>
2024-12-06 12:48:51 -0300

    att

--- daniel_testes/contador_elementos.py
+++ daniel_testes/contador_elementos.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
-
+# A = 0, B = 1, C = 2, D = 3, E = 4, F = 5, G = 6, H = 7, I = 8, J = 9, K = 10, L = 11, M = 12, N = 13, O = 14, P = 15, Q = 16, R = 17, S = 18, T = 19, U 
 # Carregar os dados
 minha_base = pd.read_csv("/home/daniel-porto/Sistemas_inteligentes/trab_tratamento/train.csv")
 
@@ -14,3 +14,13 @@ print("Número de fabricas únicas:", numero_fabricas)
 
 numero_categoria = minha_base.iloc[:,5].nunique()
 print("Número de categorias únicas:", numero_categoria)
+
+numero_cambios = minha_base.iloc[:, 11]
+print("Número de cambios únicos:", numero_cambios.nunique())
+
+numero_id=minha_base.iloc[:, 0]
+print("Número de IDs únicos:", numero_id.nunique())
+
+duplicatas = minha_base[minha_base.duplicated()]
+print("\nNúmero de duplicatas:", len(duplicatas))
+print("linhas duplicatas:", duplicatas)
