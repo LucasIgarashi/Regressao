47798fdcec8ca7ec02ee5e136691b37a059dd36d
DanielPorto09 <daniel.estudanteufsc@gmail.com>
2024-12-06 12:31:33 -0300

    att

--- daniel_testes/contador_elementos.py
+++ daniel_testes/contador_elementos.py
@@ -6,5 +6,11 @@ from sklearn.preprocessing import LabelEncoder
 minha_base = pd.read_csv("/home/daniel-porto/Sistemas_inteligentes/trab_tratamento/train.csv")
 
 # Número de amostras únicas na coluna (supondo que a coluna 3 seja "modelos")
-numero_unico = minha_base.iloc[:, 2].nunique()  # Use iloc para acessar a coluna pela posição
-print("Número de modelos únicos:", numero_unico)
+numero_modelos = minha_base.iloc[:, 3].nunique()  # Use iloc para acessar a coluna pela posição
+print("Número de modelos únicos:", numero_modelos)
+
+numero_fabricas = minha_base.iloc[:, 2].nunique() 
+print("Número de fabricas únicas:", numero_fabricas)
+
+numero_categoria = minha_base.iloc[:,5].nunique()
+print("Número de categorias únicas:", numero_categoria)
