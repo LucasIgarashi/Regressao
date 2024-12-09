73b420dcfcd7b29077e163349ecfcb7b7f63772d
DanielPorto09 <daniel.estudanteufsc@gmail.com>
2024-12-09 09:03:12 -0300

    att trab final

--- daniel_testes/Modes[Gessner].py
+++ daniel_testes/Modes[Gessner].py
@@ -1,21 +1,21 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import OneClassSVM
-from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_absolute_error
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.ensemble import GradientBoostingRegressor
 
 
 
 
 def Processar_Dados_Treino(dados):
 
-    '''EXCLUI COLUNAS INÚTEIS:''' 
+    '''EXCLUI COLUNAS INÚTEIS:''' #Ano, Combustivel, Km, Cilindros, Preco, Faixa_Preco 
     dados.drop(dados.columns[[0,1,2,3, 5,6,8,11,12,13,14,15,16,18,19,20,21,22,23]], axis=1, inplace=True)
 
 
@@ -37,18 +37,30 @@ def Processar_Dados_Treino(dados):
     dados["Km"] = dados["Km"].astype(float) # Convertendo os valores para números float
 
 
-    '''MUDAR VARIÁVEL NOMINAL PARA CONTÍNUA'''
-    #One Hot Enconding, cria uma coluna para cada categoria existente, aumenta a dimensionalidade. Usa quando tem poucas categorias, como em Combustivel, Classificacao_Veiculo e Faixa_Preco.
-    # Applying one-hot encoding
-    dados = pd.get_dummies(dados, dtype=float)
-
-
     '''PREENCHER LACUNAS'''
     #Preencher Km pela média dos Km.
     dados["Km"] = dados["Km"].fillna(dados["Km"].mean())
     #Preencher Km pela média dos Km.
     dados["Preco"] = dados["Preco"].fillna(dados["Preco"].mean())
+    
 
+    '''EXCLUI LINHAS COM DADOS FALTANTES'''
+    # Removendo linhas com valores ausentes
+    dados = dados.dropna()
+
+
+    '''MUDAR VARIÁVEL NOMINAL PARA CONTÍNUA'''
+    #One Hot Enconding, cria uma coluna para cada categoria existente, aumenta a dimensionalidade. Usa quando tem poucas categorias, como em Combustivel, Classificacao_Veiculo e Faixa_Preco.
+    faixa_preco = dados.iloc[:, -1]  # Seleciona a última coluna
+    dados = pd.get_dummies(dados.iloc[:, :-1],drop_first=True, dtype=float)
+    dados = pd.concat([dados, faixa_preco], axis=1)
+
+
+    # Transformando dados categóricos ORDINARIOS em números (faixa_preco)
+    ordem = ["Econômico", "Médio", "Luxo", "Muito Luxo"]
+    encoder = OrdinalEncoder(categories=[ordem])
+    dados['Faixa_Preco'] = encoder.fit_transform(dados[['Faixa_Preco']])
+    
 
     '''SEPARA O PREÇO(target) DOS ATRIBUTOS'''
     precos = dados["Preco"]
@@ -64,7 +76,7 @@ def Processar_Dados_Treino(dados):
     '''TIRA OUTLIERS''' 
     #Tirar o outliers depois de padronizar, pois o método OneClassSVM é sensível à outliers.
 
-    tirar_outl = OneClassSVM(kernel='rbf', gamma=0.1, nu=0.05)  # Ajustar `gamma` e `nu` conforme necessário
+    tirar_outl = OneClassSVM(kernel='rbf', gamma=0.01, nu=0.05) 
     tirar_outl.fit(atributos_padronizados)
     amostras_normais = tirar_outl.predict(atributos_padronizados) == 1 #O modelo retorna 1 para amostras consideradas normais e -1 para outliers.
 
@@ -77,14 +89,16 @@ def Processar_Dados_Treino(dados):
     dados_limpos = dados_limpos.to_numpy()
     precos_limpos = precos_limpos.to_numpy()
 
+
     return dados_limpos, precos_limpos
 
 
 
 def Processar_Dados_Teste(dados):
 
-    '''EXCLUI COLUNAS INÚTEIS:''' 
+    '''EXCLUI COLUNAS INÚTEIS:''' #Ano, Combustivel, Km, Cilindros, Preco, Classificacao_veiculo, Faixa_Preco 
     dados.drop(dados.columns[[0,1,2,3, 5,6,8,11,12,13,14,15,16,18,19,20,21,22,23]], axis=1, inplace=True)
+
 
 
     '''ARRUMAR SINTAXE'''
@@ -105,19 +119,30 @@ def Processar_Dados_Teste(dados):
     dados["Km"] = dados["Km"].astype(float) # Convertendo os valores para números float
 
 
-    '''MUDAR VARIÁVEL NOMINAL PARA CONTÍNUA'''
-    #One Hot Enconding, cria uma coluna para cada categoria existente, aumenta a dimensionalidade. Usa quando tem poucas categorias, como em Combustivel, Classificacao_Veiculo e Faixa_Preco.
-    # Applying one-hot encoding
-    dados = pd.get_dummies(dados, dtype=float)
-
-
     '''PREENCHER LACUNAS'''
-    #tem que discutir isso aqui  ass: @Gessner-Lucas
     #Preencher Km pela média dos Km.
     dados["Km"] = dados["Km"].fillna(dados["Km"].mean())
     #Preencher Km pela média dos Km.
     dados["Preco"] = dados["Preco"].fillna(dados["Preco"].mean())
+    
 
+    '''EXCLUI LINHAS COM DADOS FALTANTES'''
+    # Removendo linhas com valores ausentes
+    dados = dados.dropna()
+
+
+    '''MUDAR VARIÁVEL NOMINAL PARA CONTÍNUA'''
+    #One Hot Enconding, cria uma coluna para cada categoria existente, aumenta a dimensionalidade. Usa quando tem poucas categorias, como em Combustivel, Classificacao_Veiculo e Faixa_Preco.
+    faixa_preco = dados.iloc[:, -1]  # Seleciona a última coluna
+    dados = pd.get_dummies(dados.iloc[:, :-1],drop_first=True, dtype=float)
+    dados = pd.concat([dados, faixa_preco], axis=1)
+
+
+    # Transformando dados categóricos ORDINARIOS em números (faixa_preco)
+    ordem = ["Econômico", "Médio", "Luxo", "Muito Luxo"]
+    encoder = OrdinalEncoder(categories=[ordem])
+    dados['Faixa_Preco'] = encoder.fit_transform(dados[['Faixa_Preco']])
+    
 
     '''SEPARA O PREÇO(target) DOS ATRIBUTOS'''
     precos = dados["Preco"]
@@ -129,8 +154,6 @@ def Processar_Dados_Teste(dados):
     padronizar.fit(atributos)
     atributos_padronizados = padronizar.transform(atributos) #vira np array
 
-    '''MUDA DE VOLTA PARA NUMPY'''
-    precos = precos.to_numpy()
 
     return atributos_padronizados, precos
 
@@ -150,7 +173,7 @@ def Random_Forest(dados_treino, precos_treino, dados_teste):
 
 '''MÉTODO: SVR'''
 def Svr(dados_treino, precos_treino, dados_teste):
-    srv = SVR()
+    srv = SVR( kernel='rbf', degree=3, gamma='scale', coef0=0.0, tol=0.001, C=1.0, epsilon=0.1, shrinking=True, cache_size=200, verbose=False, max_iter=-1)
     srv.fit(dados_treino, precos_treino)
 
     #Previsão
@@ -159,16 +182,14 @@ def Svr(dados_treino, precos_treino, dados_teste):
     return precos_previstos
 
 
-'''MÉTODO: REGRESSÃO LINEAR'''
-def Regressao_Linear(dados_treino, precos_treino, dados_teste):
-    linear = LinearRegression()
-    linear.fit(dados_treino, precos_treino)
 
-    #Previsão
-    precos_previstos = linear.predict(dados_teste)
-    
-    return precos_previstos
+def Gradient(dados_treino, precos_treino, dados_teste):
+    # Create a lasso regression model
+    model = GradientBoostingRegressor( loss='squared_error', learning_rate=0.1, n_estimators=100, subsample=1.0, criterion='friedman_mse', min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_depth=3, min_impurity_decrease=0.0, init=None, random_state=None, max_features=None, alpha=0.9, verbose=0, max_leaf_nodes=None, warm_start=False, validation_fraction=0.1, n_iter_no_change=None, tol=0.0001, ccp_alpha=0.0)
+    model.fit(dados_treino, precos_treino)
 
+    # Predict the response for a new data point
+    return model.predict(dados_teste)
 
 '''R2 Ajustado''' #0 à 1
 def R2(labels_test, labels_pred,data_train):
@@ -178,8 +199,20 @@ def R2(labels_test, labels_pred,data_train):
 
     return adj_r2
 
+'''R2 Ajustado''' #0 à 1
+def R(labels_test, labels_pred):
+    return r2_score(labels_test, labels_pred)
+
+
+
+
+   
+
+
+
 '''CARREGAR OS DADOS'''
 dados = pd.read_csv("/home/daniel-porto/Sistemas_inteligentes/trab_tratamento/train.csv")
+
 
 '''SEPARA EM DADOS TESTE E TREINO:'''
 cru_treino, cru_teste = train_test_split(dados, train_size=0.5, random_state=7) 
@@ -190,19 +223,12 @@ dados_treino, precos_treino = Processar_Dados_Treino(cru_treino)
 dados_teste, precos_teste = Processar_Dados_Teste(cru_teste)
 
 
-'''MÉTODO: REGRESSÃO LINEAR'''
-precos_previstos = Regressao_Linear(dados_treino, precos_treino, dados_teste)
-print("MÉTODO: REGRESSÃO LINEAR")
-print("R2",R2(precos_teste, precos_previstos, dados_treino))
-MAE = mean_absolute_error(precos_teste, precos_previstos)
-print("MAE",MAE)  
-print("------------") 
-
 
 '''MÉTODO: SVR'''
 precos_previstos = Svr(dados_treino, precos_treino, dados_teste)
 print("MÉTODO: SVR")
 print("R2",R2(precos_teste, precos_previstos, dados_treino))
+print("R",R(precos_teste, precos_previstos))
 MAE = mean_absolute_error(precos_teste, precos_previstos)
 print("MAE",MAE)   
 print("------------") 
@@ -212,23 +238,21 @@ print("------------")
 precos_previstos = Random_Forest(dados_treino, precos_treino, dados_teste)
 print("MÉTODO: RANDOM FOREST")
 print("R2",R2(precos_teste, precos_previstos, dados_treino))
+print("R",R(precos_teste, precos_previstos))
 MAE = mean_absolute_error(precos_teste, precos_previstos)
 print("MAE",MAE) 
 print("------------") 
 
-"""
-reindex
 
-Adiciona colunas ausentes no conjunto de teste: Se Combustivel_Gás está no treino, mas não no teste,
-ele adiciona essa coluna ao teste com valores 0 (porque "Gás" não apareceu no teste).
 
-# Realiza o encoding no conjunto de treino
-dados_treino = pd.get_dummies(cru_treino, dtype=float)
+'''MÉTODO: GRADIENT'''
+precos_previstos = Gradient(dados_treino, precos_treino, dados_teste)
+print("MÉTODO: Gradient")
+print("R2",R2(precos_teste, precos_previstos, dados_treino))
+MAE = mean_absolute_error(precos_teste, precos_previstos)
+print("MAE",MAE) 
+print("------------") 
 
-# Realiza o encoding no conjunto de teste
-dados_teste = pd.get_dummies(cru_teste, dtype=float)
 
-# Reindexa o teste para alinhar as colunas com o treino
-dados_teste = dados_teste.reindex(columns=dados_treino.columns, fill_value=0)
 
-"""
+
